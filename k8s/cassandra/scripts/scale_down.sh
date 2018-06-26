a521b8e073332e2972f59f218aba1a481599d530
Jakub Przychodzeń <jprzychodzen@google.com>
2018-06-26 10:42:30 +0200

    [Cassandra] Refactor scale_down.sh

--- k8s/cassandra/scripts/scale_down.sh
+++ k8s/cassandra/scripts/scale_down.sh
@@ -9,12 +9,16 @@
 # cluster, as Cassandra has marked that this disk belongs to decommissioned
 # node. Thus, we need to delete this disk, removing PV and PVC.
 
-if [[ ! -f scripts/util.sh ]]; then
+set -eu
+
+export SCRIPT_DIR=$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")
+
+if [[ ! -f "${SCRIPT_DIR}/util.sh" ]]; then
   >&2 echo "Missing util.sh file, exiting"
   exit 1
 fi
 
-. scripts/util.sh
+. "${SCRIPT_DIR}/util.sh"
 
 DESIRED_NUMBER=${1:-1}
 
@@ -28,7 +32,7 @@ if [[ $DESIRED_NUMBER -lt 1 ]]; then
   exit 1
 fi
 
-$(current_status)
+current_status
 
 info "Scalling down to ${DESIRED_NUMBER} of replicas"
 
@@ -37,19 +41,19 @@ for node_number in $(seq $(( $(get_desired_number_of_replicas_in_sts) - 1 )) -1 
   PVC_NAME="${APP_INSTANCE_NAME}-cassandra-pvc-${APP_INSTANCE_NAME}-cassandra-${node_number}"
   PV_NAME=$(kubectl get pvc -n "${NAMESPACE}" "${PVC_NAME}" --output jsonpath='{.spec.volumeName}')
 
-  $(wait_for_healthy_sts)
+  wait_for_healthy_sts
   info "Removing Cassandra node ${node_number} from Cassandra cluster"
   kubectl exec "${POD_NAME}" --namespace "${NAMESPACE}" -c cassandra -- nodetool decommission
   info "Removing pod ${POD_NAME} from stateful set ${STS_NAME}"
   kubectl scale statefulsets "${STS_NAME}" -n "${NAMESPACE}" "--replicas=${node_number}"
-  $(wait_for_healthy_sts)
+  wait_for_healthy_sts
   info "Removing persitent volumes and persitent volume claims for pod ${POD_NAME}"
   kubectl delete "pvc/${PVC_NAME}" -n "${NAMESPACE}"
   kubectl delete "pv/${PV_NAME}" -n "${NAMESPACE}"
   info ""
 done
 
-$(wait_for_healthy_sts)
+wait_for_healthy_sts
 
 info ""
 info "Done"
