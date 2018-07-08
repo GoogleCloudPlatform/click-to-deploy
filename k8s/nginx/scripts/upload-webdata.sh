2c1348a56b255b8e196f14c795b9c597bc6073bb
Rafal Biegacz <rafalbiegacz@google.com>
2018-07-08 17:53:40 +0200

    initial version of scripts

---
+++ k8s/nginx/scripts/upload-webdata.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Copyright 2018 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -eu
+
+if [[ -z "$1" ]]; then
+  info "Please provide number of pods"
+  exit 1
+fi
+
+readonly loop_end=$1-1
+
+for ((i=0; i<=$loop_end; i++))
+do
+  command="cp html nginx-1-nginx-${i}:/usr/share/nginx/html"
+  echo "Executing: $command" 
+  kubectl cp html nginx-1-nginx-${i}:/usr/share/nginx
+  kubectl exec nginx-1-nginx-${i} -- chmod -R a+r /usr/share/nginx/html
+done
+
