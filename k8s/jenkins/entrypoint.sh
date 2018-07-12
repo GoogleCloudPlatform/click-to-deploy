3714ef86e58f87edf0398d0aeb32ae397e58c791
Tomasz Safuryn <tsafuryn@google.com>
2018-07-12 16:54:13 +0200

    Added tests.

--- k8s/jenkins/entrypoint.sh
+++ k8s/jenkins/entrypoint.sh
@@ -12,7 +12,7 @@ kubectl --namespace $namespace create secret generic $name-tls \
 /bin/deploy-org.sh
 
 application_uid=$(kubectl get applications/$name --namespace=$namespace --output=jsonpath='{.metadata.uid}')
-kubectl patch secret $name-tls -p "{\"metadata\":{\"ownerReferences\":[{\"apiVersion\":\"app.k8s.io/v1alpha1\",\"blockOwnerDeletion\":true,\"kind\":\"Application\",\"name\":\"$name\",\"uid\":\"$application_uid\"}]}}"
+kubectl patch secret $name-tls -p "{\"metadata\":{\"ownerReferences\":[{\"apiVersion\":\"app.k8s.io/v1beta1\",\"blockOwnerDeletion\":true,\"kind\":\"Application\",\"name\":\"$name\",\"uid\":\"$application_uid\"}]}}"
 
 NAME="$(/bin/print_config.py --param '{"x-google-marketplace": {"type": "NAME"}}')"
 NAMESPACE="$(/bin/print_config.py --param '{"x-google-marketplace": {"type": "NAMESPACE"}}')"
