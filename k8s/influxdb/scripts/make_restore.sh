f0358ab657b9caa282e0056047b5e781be21fad7
Rafal Biegacz <rafalbiegacz@google.com>
2018-07-15 15:27:32 +0200

    scripts for backup and restore added

---
+++ k8s/influxdb/scripts/make_restore.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Copyright 2018 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -eu
+
+if [[ -z "$1" ]]; then
+  info "Please provide InfluxDB instance name"
+  exit 1
+fi
+
+if [[ -z "$2" ]]; then
+  NAMESPACE=default
+else
+  NAMESPACE=$2
+fi
+
+INFLUXDB_INSTANCE="$1"
+INFLUXDB_BACKUP_DIR=influxdb-backup
+
+echo "Connecting to InfluxDB instance and creating restore dirctory."
+kubectl exec -it $INFLUXDB_INSTANCE-influxdb-0 --namespace "$NAMESPACE" -- mkdir /$INFLUXDB_BACKUP_DIR
+echo "Connecting to InfluxDB instance and performing restore operation"
+kubectl exec -it $INFLUXDB_INSTANCE-influxdb-0 --namespace "$NAMESPACE" -- influxd restore -portable /$INFLUXDB_BACKUP_DIR
+#kubectl exec -it $INFLUXDB_INSTANCE-influxdb-0 --namespace "$NAMESPACE" -- rmdir -rf /$INFLUXDB_BACKUP_DIR
