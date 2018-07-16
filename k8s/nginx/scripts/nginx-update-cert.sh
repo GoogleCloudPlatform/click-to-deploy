b947bf07e8cc8afe6bbbae47f8b7770abdb0711f
Rafal Biegacz <rafalbiegacz@google.com>
2018-07-16 10:42:56 +0200

    script for secret update

---
+++ k8s/nginx/scripts/nginx-update-cert.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#
+# Copyright 2018 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+CERT_FILE=https1.cert
+KEY_FILE=https1.key
+
+if [[ -z "$NAMESPACE" ]]; then
+  echo "Define NAMESPACE environment variable!"
+  exit 1
+fi
+
+if [[ -z "$APP_INSTANCE_NAME" ]]; then
+  echo "Define APP_INSTANCE_NAME environment variable!"
+  exit 1
+fi
+
+if [[ -f $CERT_FILE ]]; then
+  echo "Using $CERT_FILE as a certificate."
+else
+  echo "File $CERT_FILE doesn't exist. Run nginx-create-key.sh script"
+  exit 1
+fi
+
+if [[ -f $KEY_FILE ]]; then
+  echo "Using $KEY_FILE as a certificate."
+else
+  echo "File $KEY_FILE doesn't exist. Run nginx-create-key.sh script"
+  exit 1
+fi
+
+kubectl --namespace $NAMESPACE create secret generic $APP_INSTANCE_NAME-nginx-secret \
+	--from-file=$CERT_FILE --from-file=$KEY_FILE \
+	--dry-run -o yaml | kubectl apply -f -
