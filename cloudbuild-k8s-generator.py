f8632f129fb0ac79f764ecb62c27245b67941030
Yinan Li <ynli@google.com>
2019-04-24 09:40:48 -0700

    Fixed spark-operator app/verify (#523)

--- cloudbuild-k8s-generator.py
+++ cloudbuild-k8s-generator.py
@@ -181,9 +181,8 @@ def main():
       help='verify %s file' % CLOUDBUILD_CONFIG)
   args = parser.parse_args()
 
-  # TODO(PR/346): Spark operator: "make app/verify" fails
   # TODO(PR/516): SonarQube is flaky
-  skiplist = ['sonarqube', 'spark-operator']
+  skiplist = ['sonarqube']
 
   # Use extra_configs to run additional deployments
   # with non-default configurations.
