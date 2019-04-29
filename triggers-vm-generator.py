aa091e250c4c93008f3088aa56ede8dc2d30e9ec
Wojciech Grzelak <wgrzelak@google.com>
2019-04-29 13:34:18 +0200

    Small refactoring

--- triggers-vm-generator.py
+++ triggers-vm-generator.py
@@ -1,3 +1,19 @@
+#!/usr/bin/env python
+#
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 import docker
 import yaml
@@ -55,13 +71,17 @@ class GenerateTriggerConfig():
         included_files.append(os.path.join('vm/chef', dep, '**'))
 
     included_files = self.remove_duplicates(included_files)
-    included_files.sort()
     return included_files
 
   def remove_duplicates(self, included_files):
-    return list(dict.fromkeys(included_files))
+    final_list = []
+    for num in included_files:
+      if num not in final_list:
+        final_list.append(num)
+    return final_list
 
   def generate_config(self, included_files):
+    included_files.sort()
     trigger = {
         'description': 'Trigger for VM %s' % self._solution,
         'filename': CLOUDBUILD_CONFIG,
