8d2db5b12d0e9ef1ba1a89b303693131b1a42ce5
Joe Sullivan <jmsullivan@google.com>
2019-09-17 17:54:29 -0700

    Removed xpack install
    xpack is installed by default

--- vm/chef/cookbooks/elasticsearch/attributes/default.rb
+++ vm/chef/cookbooks/elasticsearch/attributes/default.rb
@@ -1,4 +1,4 @@
-# Copyright 2018 Google LLC
+# Copyright 2019 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,6 +21,3 @@ default['elasticsearch']['repository_url'] =
   "https://artifacts.elastic.co/packages/#{default['elasticsearch']['release']}.x/apt"
 default['elasticsearch']['keyserver_url'] =
   'https://artifacts.elastic.co/GPG-KEY-elasticsearch'
-
-default['elasticsearch']['x-pack']['download_url'] =
-  "https://artifacts.elastic.co/downloads/packs/x-pack/x-pack-#{default['elasticsearch']['version']}.zip"
