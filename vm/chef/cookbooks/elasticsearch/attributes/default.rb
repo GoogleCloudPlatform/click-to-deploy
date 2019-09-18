2bcfdff9d6bfcabb58fc45165f31383f9344783f
Joe Sullivan <jmsullivan@google.com>
2019-09-17 17:43:19 -0700

    Bump elasticsearch to 7.3.2

--- vm/chef/cookbooks/elasticsearch/attributes/default.rb
+++ vm/chef/cookbooks/elasticsearch/attributes/default.rb
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-default['elasticsearch']['version'] = '5.6.8'
+default['elasticsearch']['version'] = '7.3.2'
 
 default['elasticsearch']['release'] =
   default['elasticsearch']['version'].split('.')[0]
