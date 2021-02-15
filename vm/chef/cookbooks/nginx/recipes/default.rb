f4dced891e1312df6bab86ffd9f8bdaea2a8f9f7
Armando Miani <armadom@google.com>
2021-02-15 17:31:02 +0100

    VM - NGINX - Update NGINX from 1.10 to 1.14

--- vm/chef/cookbooks/nginx/recipes/default.rb
+++ vm/chef/cookbooks/nginx/recipes/default.rb
@@ -12,9 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-package 'install packages' do
+package 'install packages' do # ~FC009
   package_name node['nginx']['packages']
+  default_release 'stretch-backports'
   action :install
+  retries 5
+  retry_delay 60
 end
 
 execute 'rename index.html' do
