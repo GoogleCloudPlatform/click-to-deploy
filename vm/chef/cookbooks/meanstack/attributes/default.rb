c9da60d51a3c754dd510362c253a4ea737b28034
Armando Miani <armadom@google.com>
2021-02-18 13:50:16 +0100

    VM - Add MEAN Stack Solution

--- vm/chef/cookbooks/meanstack/attributes/default.rb
+++ vm/chef/cookbooks/meanstack/attributes/default.rb
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-default['shit']['chat'] = 'hu'
+default['meanstack']['angular']['version'] = '11.0.2'
