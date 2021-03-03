1fb1aa297d4c760dc171bc62149bb505b7c869ca
Armando Miani <armadom@google.com>
2021-03-03 13:21:23 +0100

    Remove unused variable

--- vm/chef/cookbooks/opencart/attributes/default.rb
+++ vm/chef/cookbooks/opencart/attributes/default.rb
@@ -11,10 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-# Reference: https://www.drupal.org/download
 
-default['opencart']['user'] = 'www-data'
 default['opencart']['version'] = '3.0.3.6'
 default['opencart']['db']['name'] = 'opencart'
 default['opencart']['packages'] = ['jq']
