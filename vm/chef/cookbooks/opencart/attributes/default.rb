658ec021a4d40cb5ca89b27497b91ed95d419e31
Armando Miani <armadom@google.com>
2021-03-04 13:45:21 +0100

    Fixes

--- vm/chef/cookbooks/opencart/attributes/default.rb
+++ vm/chef/cookbooks/opencart/attributes/default.rb
@@ -12,6 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-default['opencart']['version'] = '3.0.3.6'
+default['opencart']['version'] = '3.0.3.7'
 default['opencart']['db']['name'] = 'opencart'
 default['opencart']['packages'] = ['jq']
