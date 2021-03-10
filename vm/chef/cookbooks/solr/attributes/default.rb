5d5d92bae98c329d1d3a4ac5a2a4ee0af0bf445e
Armando Miani <armadom@google.com>
2021-03-10 22:02:13 +0100

    Fix version

--- vm/chef/cookbooks/solr/attributes/default.rb
+++ vm/chef/cookbooks/solr/attributes/default.rb
@@ -12,7 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require 'net/http'
-
 default['solr']['version'] = '8.8.1'
 default['solr']['packages'] = ['lsof']
