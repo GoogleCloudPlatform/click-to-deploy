1b563424cd6ab155ed863b6453bbf9e622b6ef83
Armando Miani <armadom@google.com>
2021-03-10 11:20:04 +0100

    VM - Solr - Dynamically get the latest version

--- vm/chef/cookbooks/solr/attributes/default.rb
+++ vm/chef/cookbooks/solr/attributes/default.rb
@@ -1,4 +1,4 @@
-# Copyright 2018 Google LLC
+# Copyright 2021 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,5 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-default['solr']['version'] = '8.6.2'
+require 'net/http'
+
+# Find latest version for Solr 8
+uri = URI('https://downloads.apache.org/lucene/solr/')
+response = Net::HTTP.get(uri)
+match = response.match /(?<version>8\.\d+\.\d+)/
+
+default['solr']['version'] = match[:version]
 default['solr']['packages'] = ['lsof']
