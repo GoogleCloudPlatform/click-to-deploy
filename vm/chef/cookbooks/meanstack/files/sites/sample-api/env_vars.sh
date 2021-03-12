61f2f5160ed19e0b9c1ff7ff58928021748bf948
Armando Miani <armadom@google.com>
2021-03-12 16:03:53 +0100

    VM - MEAN - Fix folders

--- vm/chef/cookbooks/meanstack/files/sites/sample-api/env_vars.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source /opt/c2d/c2d-utils || exit 1
-
-readonly mongodb_password="$(get_attribute_value "mongodb_admin_password")"
-
-export MONGODB_USERNAME="admin"
-export MONGODB_PASSWORD="${mongodb_password}"
-export MONGODB_NAME="test"
