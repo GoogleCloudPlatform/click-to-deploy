eb3d983a164ccd5bdf8b29440ac0daeb1c0d5c26
ovk6 <kalinichenka@google.com>
2021-03-17 18:19:36 +0100

    Fixed erpnext VM cookbook

--- vm/chef/cookbooks/erpnext/attributes/default.rb
+++ vm/chef/cookbooks/erpnext/attributes/default.rb
@@ -13,16 +13,22 @@
 # limitations under the License.
 
 default['erpnext']['packages'] = [
-    'python3-minimal',
+    'build-essential',
+    'git',
+    'libmariadb-dev',
+    'mariadb-server-10.3',
+    'nginx',
+    'python3-dev',
+    'python3-pip',
     'python3-setuptools',
     'redis-server',
-    'nginx',
-    'build-essential',
+    'supervisor',
+    'virtualenv',
+    'wkhtmltopdf',
 ]
 
 default['erpnext']['version'] = '12'
 default['erpnext']['site'] = 'site1.local'
-default['erpnext']['install-script'] = 'https://raw.githubusercontent.com/frappe/bench/master/playbooks/install.py'
 
 default['erpnext']['frappe']['bench'] = 'frappe-bench'
 default['erpnext']['frappe']['user'] = 'frappe'
@@ -30,5 +36,3 @@ default['erpnext']['frappe']['password'] = `openssl rand -base64 12 | fold -w 12
 
 default['erpnext']['mysql-root-password'] = `openssl rand -base64 12 | fold -w 12 | head -n1 | tr -d '\r\n'`
 default['erpnext']['erpnext-admin-password'] = `openssl rand -base64 12 | fold -w 12 | head -n1 | tr -d '\r\n'`
-
-default['erpnext']['werkzeug']['version'] = '0.16.1'
