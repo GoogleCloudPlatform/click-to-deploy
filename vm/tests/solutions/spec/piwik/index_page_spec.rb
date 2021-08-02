339a064eef3ead86c396b8e3b61e211074b462f5
Yauhen Karalevich <yauhenk@google.com>
2021-08-02 11:27:51 +0000

    Piwik  - test change

--- vm/tests/solutions/spec/piwik/index_page_spec.rb
+++ vm/tests/solutions/spec/piwik/index_page_spec.rb
@@ -15,6 +15,6 @@
 require 'spec_helper'
 
 describe command('curl -L http://localhost/') do
-  its(:stdout) { should match /<title>Matomo (/\d/) &rsaquo; Installation<\/title>/ }
+  its(:stdout) { should match /<<title>Matomo (\d).(\d).(\d) &rsaquo; Installation<\/title>/ }
   its(:exit_status) { should eq 0 }
 end
