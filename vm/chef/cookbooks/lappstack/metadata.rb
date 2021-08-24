0163d77815646f3739f84bc83eb9c82520c724ad
Armando Miani <armadom@google.com>
2021-08-24 15:39:11 +0200

    Move lapp to lappstack folder

--- vm/chef/cookbooks/lappstack/metadata.rb
+++ vm/chef/cookbooks/lappstack/metadata.rb
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-name 'lapp'
+name 'lappstack'
 supports 'debian'
 depends 'apache2'
 depends 'postgresql'
