b7c76a5146323b030f0cd40eed06710cc8d7ba00
Armando Miani <armadom@google.com>
2021-08-27 12:22:16 +0200

    fix java setup

--- vm/chef/cookbooks/alfresco/recipes/default.rb
+++ vm/chef/cookbooks/alfresco/recipes/default.rb
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include_recipe 'openjdk8'
+include_recipe 'openjdk11'
 include_recipe 'apache2'
 include_recipe 'apache2::rm-index'
 include_recipe 'apache2::security-config'
