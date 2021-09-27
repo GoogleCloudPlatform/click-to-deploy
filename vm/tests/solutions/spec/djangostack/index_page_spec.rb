36378c51b09ef45674665453f6e3b21e1e99a16a
Yauhen Karalevich <yauhenk@google.com>
2021-09-27 17:40:42 +0000

    [vm]Django - update to debian 10, startup fix

--- vm/tests/solutions/spec/djangostack/index_page_spec.rb
+++ vm/tests/solutions/spec/djangostack/index_page_spec.rb
@@ -14,7 +14,7 @@
 
 require 'spec_helper'
 
-describe command("curl -L http://#{:ipaddress}/") do
+describe command('curl -L http://localhost/') do
   its(:stdout) { should match /<title>The install worked successfully! Congratulations!<\/title>/ }
   its(:exit_status) { should eq 0 }
 end
