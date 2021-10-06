ab6d4c2631b20f2db197dd7283257ca668e9c8cc
Yauhen Karalevich <yauhenk@google.com>
2021-10-06 10:57:28 +0000

    Alfresco - openjdk11 cookbook fix for Debian10

--- vm/chef/cookbooks/alfresco/recipes/default.rb
+++ vm/chef/cookbooks/alfresco/recipes/default.rb
@@ -58,10 +58,6 @@ apt_package 'wget' do
   action :install
 end
 
-apt_package 'default-jdk' do
-  action :install
-end
-
 bash 'download_and_check_alfresco' do
   code <<-EOH
     wget --no-check-certificate $alfresco_install_url -O /tmp/alfresco.bin
