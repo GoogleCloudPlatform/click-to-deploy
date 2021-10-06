bcaa29208f445252b0ae7c8d29fbde2957702454
Yauhen Karalevich <yauhenk@google.com>
2021-10-06 09:47:52 +0000

    Cassandra - rever to version 3.11.11

--- vm/chef/cookbooks/cassandra/recipes/default.rb
+++ vm/chef/cookbooks/cassandra/recipes/default.rb
@@ -18,7 +18,7 @@ apt_repository 'cassandra_repository' do
   uri node['cassandra']['repo']['uri']
   components node['cassandra']['repo']['components']
   keyserver node['cassandra']['repo']['keyserver']
-  distribution '40x'
+  distribution '311x'
   trusted true
 end
 
