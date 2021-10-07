f5499594c0ecb973d817dc0bf2c79b8deba1a254
ovk6 <kalinichenka@google.com>
2021-10-07 11:27:57 +0200

    Added retries

--- vm/chef/cookbooks/php80/recipes/module_xml.rb
+++ vm/chef/cookbooks/php80/recipes/module_xml.rb
@@ -12,4 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-package 'php8.0-xml'
+package 'php8.0-xml' do
+  :install
+  retries 5
+  retry_delay 60
+end
