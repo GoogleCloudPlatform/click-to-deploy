c7302d0fd72dd1b12d3de73417ec43ecb19849b6
Yauhen Karalevich <yauhenk@google.com>
2021-10-08 13:11:11 +0000

    Zabbix - fix chef 17 compatibilities issues

--- vm/chef/cookbooks/zabbix/recipes/default.rb
+++ vm/chef/cookbooks/zabbix/recipes/default.rb
@@ -17,19 +17,18 @@ include_recipe 'apache2::default'
 include_recipe 'apache2::rm-index'
 include_recipe 'apache2::security-config'
 
-apt_package 'wget' do
-  action :install
+# install zabbix package
+apt_repository 'zabbix' do
+  uri node['zabbix']['repo']['uri']
+  components node['zabbix']['repo']['components']
+  distribution node['zabbix']['repo']['distribution']
+  key node['zabbix']['repo']['keyserver']
 end
 
-# install zabbix package
-bash 'install zabbix' do
-  user 'root'
-  cwd '/tmp'
-  code <<-EOH
-  wget https://repo.zabbix.com/zabbix/#{node['zabbix']['version']}/debian/pool/main/z/zabbix-release/zabbix-release_#{node['zabbix']['release']}+buster_all.deb
-  dpkg -i zabbix-release_#{node['zabbix']['release']}+buster_all.deb
-  apt-get update
-EOH
+apt_update 'update' do
+  action :update
+  retries 5
+  retry_delay 30
 end
 
 package 'install packages' do
