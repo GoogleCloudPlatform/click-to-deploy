d518e8c79bf14f73003540ba6a9b61e32c747223
Yauhen Karalevich <yauhenk@google.com>
2021-12-24 20:54:09 +0000

    Weblate  - fixes and update to Debian 11

--- vm/chef/cookbooks/postgresql/recipes/standalone_buster.rb
+++ vm/chef/cookbooks/postgresql/recipes/standalone_buster.rb
@@ -12,6 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-node.override['postgresql']['standalone']['distribution'] = 'bullseye'
+node.override['postgresql']['standalone']['distribution'] = 'buster'
 
 include_recipe 'postgresql::standalone'
