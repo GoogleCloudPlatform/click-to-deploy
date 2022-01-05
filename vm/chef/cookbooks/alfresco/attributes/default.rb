79394fcb8c9d3eca88666f44ab8bc941f2ec4f61
Yauhen Karalevich <yauhenk@google.com>
2022-01-05 10:42:55 +0000

    Alfresco - version upgrdade

--- vm/chef/cookbooks/alfresco/attributes/default.rb
+++ vm/chef/cookbooks/alfresco/attributes/default.rb
@@ -1,4 +1,4 @@
-# Copyright 2021 Google LLC
+# Copyright 2022 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,12 +12,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-default['alfresco']['install']['url'] = 'https://download.alfresco.com/cloudfront/release/community/7.0.0-build-2355/alfresco-content-services-community-distribution-7.0.0.zip'
-default['alfresco']['install']['sha256'] = 'f961b1de1756d88b0cce9e856c1f63c670bd82372b803c6ee9bb6d103f3393ca'
+default['alfresco']['install']['url'] = 'https://download.alfresco.com/cloudfront/release/community/7.1.0.1-build-2669/alfresco-content-services-community-distribution-7.1.0.1.zip'
+default['alfresco']['install']['sha256'] = '8a7036b72134a7e685b494f776f3dd1576b380d1ac563ee78d38b2a88107b3f2'
 
 
 default['alfresco']['search']['install']['url'] = 'https://download.alfresco.com/cloudfront/release/community/SearchServices/2.0.1/alfresco-search-services-2.0.1.zip'
 default['alfresco']['search']['install']['sha256'] = '00decf904bf99a0a3dcf4bdfb830f4b3be71cea237683084d5527010574f59be'
+
+default['activemq']['install']['url'] = 'https://downloads.apache.org/activemq/5.16.3/apache-activemq-5.16.3-bin.zip'
+default['activemq']['install']['sha256'] = '0aeceabce625dde1b2d9285311dca8e0cd4abf8f384ff1e9983ca775f1f762b4'
+
+
+default['rm']['install']['url'] = 'https://download.alfresco.com/cloudfront/release/community/RM/3.0.a/alfresco-community.zip'
+default['rm']['install']['sha256'] = '6bd10de34698f2edb4811e16546cd1c63afb24438379765a2ecc0a76daad2b2d'
+
 
 default['alfresco']['db']['username'] = 'alfresco'
 default['alfresco']['db']['password'] = 'alfresco'
--- vm/chef/cookbooks/alfresco/recipes/default.rb
+++ vm/chef/cookbooks/alfresco/recipes/default.rb
@@ -1,4 +1,4 @@
-# Copyright 2021 Google LLC
+# Copyright 2022 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -60,12 +60,11 @@ apt_package 'zip' do
 end
 
 # Download alfresco.
-remote_file '/tmp/tomcat.zip' do
+remote_file '/tmp/alfresco.zip' do
   source "#{node['alfresco']['install']['url']}"
   verify "echo '#{node['alfresco']['install']['sha256']} %{path}' | sha256sum -c"
   action :create
 end
-
 
 # Extract alfreso to home directory.
 bash 'Extract Alfresco' do
@@ -73,6 +72,62 @@ bash 'Extract Alfresco' do
   cwd '/tmp'
   code <<-EOH
 unzip alfresco.zip -d /opt/alfresco
+EOH
+end
+
+# Download SearchServices.
+remote_file '/tmp/solr.zip' do
+  source "#{node['alfresco']['search']['install']['url']}"
+  verify "echo '#{node['alfresco']['search']['install']['sha256']} %{path}' | sha256sum -c"
+  action :create
+end
+
+# Extract SearchServices to home directory.
+bash 'Extract Solr' do
+  user 'root'
+  cwd '/tmp'
+  code <<-EOH
+unzip solr.zip -d /opt/solr
+EOH
+end
+
+# Download Records Management.
+remote_file '/tmp/rm.zip' do
+  source "#{node['rm']['install']['url']}"
+  verify "echo '#{node['rm']['install']['sha256']} %{path}' | sha256sum -c"
+  action :create
+end
+
+directory '/opt/alfresco/rm-apts' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  recursive true
+  action :create
+end
+
+# Extract Records Management to home directory.
+bash 'Extract Records Management' do
+  user 'root'
+  cwd '/tmp'
+  code <<-EOH
+unzip rm.zip -d /opt/alfresco/rm-apts
+EOH
+end
+
+# Download ActiveMQ.
+remote_file '/tmp/activemq.zip' do
+  source "#{node['activemq']['install']['url']}"
+  verify "echo '#{node['activemq']['install']['sha256']} %{path}' | sha256sum -c"
+  action :create
+end
+
+# Extract ActiveMQ to home directory.
+bash 'Extract ActiveMQ' do
+  user 'root'
+  cwd '/tmp'
+  code <<-EOH
+unzip activemq.zip -d /opt/activemq
 EOH
 end
 
@@ -96,25 +151,24 @@ service 'apache2' do
   action :restart
 end
 
-# Set environment variables for Alfresco's tomcat with new configuration script
-execute 'move_original_setenv_file' do
-  cwd '/opt/alfresco/tomcat/bin'
-  command 'mv setenv.sh setenv.sh.orig'
-end
-
-template '/opt/alfresco/tomcat/bin/setenv.sh' do
-  source 'setenv.sh.erb'
-  owner 'root'
-  group 'root'
-  mode '0640'
-end
-
 # Copy alfresco-global.properties template configuration file (to override):
 template '/opt/alfresco/alfresco-global.properties.template' do
   source 'alfresco-global.properties.template.erb'
   owner 'root'
   group 'root'
   mode '0640'
+end
+
+directory '/opt/tomcat/shared/classes' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  recursive true
+  action :create
+end
+
+service 'tomcat' do
+  action :stop
 end
 
 # Prepare Alfresco to be run as service
@@ -125,6 +179,4 @@ template '/etc/init.d/alfresco' do
   mode '0755'
 end
 
-# Copy post-deploy configuration script
-# (to override and configure instance's specific passwords):
 c2d_startup_script 'alfresco'
