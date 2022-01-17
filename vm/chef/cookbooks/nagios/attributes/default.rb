f81bb6335fb04058a533689e9ffc41ca231fe4e7
Yauhen Karalevich <yauhenk@google.com>
2022-01-17 08:59:52 +0000

    VM - Nagios - upgrade to version 5.8.7(nagiosXI)

--- vm/chef/cookbooks/nagios/attributes/default.rb
+++ vm/chef/cookbooks/nagios/attributes/default.rb
@@ -1,4 +1,4 @@
-# Copyright 2018 Google LLC
+# Copyright 2022 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,26 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-default['nagios']['core']['version'] = '4.4.4'
-default['nagios']['plugins']['version'] = '2.2.1'
-default['nagios']['nrpe']['version'] = '3.2.1'
+default['nagios']['version'] = '5.8.7'
 
 default['nagios']['config']['adminuser'] = 'nagiosadmin'
 
-default['nagios']['core']['dir'] = "nagios-#{node['nagios']['core']['version']}"
-default['nagios']['core']['file'] = "#{node['nagios']['core']['dir']}.tar.gz"
-default['nagios']['core']['url'] = "https://assets.nagios.com/downloads/nagioscore/releases/#{node['nagios']['core']['file']}"
-
-default['nagios']['plugins']['dir'] = "nagios-plugins-#{node['nagios']['plugins']['version']}"
-default['nagios']['plugins']['file'] = "#{node['nagios']['plugins']['dir']}.tar.gz"
-default['nagios']['plugins']['url'] = "https://nagios-plugins.org/download/#{node['nagios']['plugins']['file']}"
-
-default['nagios']['nrpe']['dir'] = "nrpe-#{node['nagios']['nrpe']['version']}"
-default['nagios']['nrpe']['file'] = "#{node['nagios']['nrpe']['dir']}.tar.gz"
-default['nagios']['nrpe']['url'] = "https://github.com/NagiosEnterprises/nrpe/releases/download/#{node['nagios']['nrpe']['dir']}/#{node['nagios']['nrpe']['file']}"
-
-default['nagios']['ncpa']['file'] = 'check_ncpa.tar.gz'
-default['nagios']['ncpa']['url'] = "https://assets.nagios.com/downloads/ncpa/#{node['nagios']['ncpa']['file']}"
-
-default['nagios']['packages']['core'] = %w(autoconf gcc libc6 libgd-dev mailutils make php unzip)
-default['nagios']['packages']['plugins'] = %w(autoconf bc build-essential dc gawk gcc gettext libc6 libmcrypt-dev libnet-snmp-perl libssl-dev make snmp wget)
+default['nagios']['dir'] = "xi-#{node['nagios']['version']}"
+default['nagios']['file'] = "#{node['nagios']['dir']}.tar.gz"
+default['nagios']['url'] = "https://assets.nagios.com/downloads/nagiosxi/5/xi-#{node['nagios']['version']}.tar.gz"
--- vm/chef/cookbooks/nagios/recipes/default.rb
+++ vm/chef/cookbooks/nagios/recipes/default.rb
@@ -1,4 +1,4 @@
-# Copyright 2018 Google LLC
+# Copyright 2022 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,107 +21,29 @@ apt_update do
   action :update
 end
 
-package 'install packages core' do
-  package_name node['nagios']['packages']['core']
-  action :install
-end
-
-package 'install packages plugins' do
-  package_name node['nagios']['packages']['plugins']
-  action :install
-end
-
-# download nagios core
-remote_file "/tmp/#{node['nagios']['core']['file']}" do
-  source node['nagios']['core']['url']
+# download nagios
+remote_file "/tmp/#{node['nagios']['file']}" do
+  source node['nagios']['url']
   action :create
 end
 
-# download nagios-plugins
-remote_file "/tmp/#{node['nagios']['plugins']['file']}" do
-  source node['nagios']['plugins']['url']
-  action :create
-end
-
-# download NRPE plugin
-remote_file "/tmp/#{node['nagios']['nrpe']['file']}" do
-  source node['nagios']['nrpe']['url']
-  action :create
-end
-
-# download NCPA plugin
-remote_file "/tmp/#{node['nagios']['ncpa']['file']}" do
-  source node['nagios']['ncpa']['url']
-  action :create
-end
-
-template '/etc/apache2/sites-available/nagios-http-redirect.conf' do
-  source 'nagios-http-redirect-conf.erb'
+template '/etc/apache2/sites-available/apache-nagios.conf' do
+  source 'apache-nagios-conf.erb'
   cookbook 'nagios'
   owner 'root'
   group 'root'
   mode '0644'
 end
 
-# configure and build nagios
+# configure and install nagios
 bash 'configure nagios' do
   user 'root'
   cwd '/tmp'
   code <<-EOH
     # copy sources
-    cp '#{node['nagios']['core']['file']}' /usr/src/
-    cp '#{node['nagios']['plugins']['file']}' /usr/src/
-    cp '#{node['nagios']['nrpe']['file']}' /usr/src/
-    cp '#{node['nagios']['ncpa']['file']}' /usr/src/
+    cp '#{node['nagios']['file']}' /usr/src/
     cd /usr/src/
-    tar -xf '#{node['nagios']['core']['file']}'
-    tar -xf '#{node['nagios']['plugins']['file']}'
-    tar -xf '#{node['nagios']['nrpe']['file']}'
-    cd -
-
-    # install nagios core
-    tar -xf '#{node['nagios']['core']['file']}'
-    cd '#{node['nagios']['core']['dir']}'
-    ./configure -with-httpd-conf=/etc/apache2/sites-enabled
-    make all
-
-    make install-groups-users
-    usermod -a -G nagios www-data
-
-    make install
-    make install-daemoninit
-
-    make install-commandmode
-    make install-config
-    make install-webconf
-    cd ../
-
-    # install nagios-plugins
-    tar -xf '#{node['nagios']['plugins']['file']}'
-    cd '#{node['nagios']['plugins']['dir']}'
-    ./tools/setup
-    ./configure
-    make all
-    make install
-    cd ../
-
-    # install NRPE plugin
-    tar -xf '#{node['nagios']['nrpe']['file']}'
-    cd '#{node['nagios']['nrpe']['dir']}'
-    PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin ./configure
-    make all
-    make install-plugin
-    cd ../
-
-    # install NCPA plugin
-    tar -xf '#{node['nagios']['ncpa']['file']}'
-    cp check_ncpa.py /usr/local/nagios/libexec
-    chown root:nagios /usr/local/nagios/libexec/check_ncpa.py
-    chmod 755 /usr/local/nagios/libexec/check_ncpa.py
-
-    a2enmod cgi ssl
-    a2dissite 000-default
-    a2ensite default-ssl nagios-http-redirect
+    tar -xzf '#{node['nagios']['file']}'
 EOH
 end
 
@@ -129,11 +51,16 @@ service 'apache2' do
   action [ :enable ]
 end
 
-service 'nagios' do
-  action [ :enable ]
+c2d_startup_script 'nagios'
+
+execute 'enable apache modules' do
+  command 'a2enmod cgi ssl'
 end
 
-c2d_startup_script 'nagios' do
-  source 'opt-c2d-scripts-nagios.erb'
-  action :template
+execute 'disable apache default.conf' do
+  command 'a2dissite 000-default'
+end
+
+execute 'enable nagios.conf' do
+  command 'a2ensite default-ssl apache-nagios'
 end
