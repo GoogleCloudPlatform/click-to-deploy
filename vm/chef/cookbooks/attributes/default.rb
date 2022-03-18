2fb24e4bea7c963f0cfdc8f0b4508b2c3810a36b
Yauhen Karalevich <yauhenk@google.com>
2022-03-18 15:40:22 +0000

    VM - OroCRM - update to version 5.0.2

--- vm/chef/cookbooks/attributes/default.rb
+++ vm/chef/cookbooks/attributes/default.rb
@@ -1,4 +1,4 @@
-# Copyright 2022 Google LLC
+# Copyright 2021 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,11 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-name 'orocrm'
-depends 'apache2'
-depends 'mysql'
-depends 'php81'
-depends 'git'
-depends 'composer'
-depends 'nodejs'
-supports 'debian'
+default['php80']['packages'] = ['php8.0-fpm', 'php8.0-common', 'php8.0']
+
+# Default modules to install
+default['php80']['modules'] = %w(cgi gd curl)
