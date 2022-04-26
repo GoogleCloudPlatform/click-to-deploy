3773ff0956ee79a4f9656b2b7df5f92d7dd1e33b
Dmitrii Ganochenko <dmitriig@google.com>
2022-04-26 13:09:56 +0000

    finalize docker image

--- docker/airflow/2/debian11/2.2/airflow/scripts/docker/install_mysql.sh
+++ docker/airflow/2/debian11/2.2/airflow/scripts/docker/install_mysql.sh
@@ -1,36 +1,16 @@
 #!/usr/bin/env bash
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
+
 set -euo pipefail
 declare -a packages
 
 MYSQL_VERSION="8.0"
 readonly MYSQL_VERSION
 
-COLOR_BLUE=$'\e[34m'
-readonly COLOR_BLUE
-COLOR_RESET=$'\e[0m'
-readonly COLOR_RESET
-
 : "${INSTALL_MYSQL_CLIENT:?Should be true or false}"
 
 install_mysql_client() {
     echo
-    echo "${COLOR_BLUE}Installing mysql client version ${MYSQL_VERSION}${COLOR_RESET}"
+    echo "Installing mysql client version ${MYSQL_VERSION}"
     echo
 
     if [[ "${1}" == "dev" ]]; then
--- docker/airflow/templates/scripts/prod/clean-logs.sh
+++ docker/airflow/templates/scripts/prod/clean-logs.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -14,9 +16,25 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-# shellcheck disable=SC1091
-source "/opt/bats/lib/bats-support/load.bash"
-# shellcheck disable=SC1091
-source "/opt/bats/lib/bats-assert/load.bash"
-# shellcheck disable=SC1091
-source "/opt/bats/lib/bats-file/load.bash"
+
+set -euo pipefail
+
+readonly DIRECTORY="${AIRFLOW_HOME:-/usr/local/airflow}"
+readonly RETENTION="${AIRFLOW__LOG_RETENTION_DAYS:-15}"
+
+trap "exit" INT TERM
+
+readonly EVERY=$((15*60))
+
+echo "Cleaning logs every $EVERY seconds"
+
+while true; do
+  echo "Trimming airflow logs to ${RETENTION} days."
+  find "${DIRECTORY}"/logs \
+    -type d -name 'lost+found' -prune -o \
+    -type f -mtime +"${RETENTION}" -name '*.log' -print0 | \
+    xargs -0 rm -f
+
+  seconds=$(( $(date -u +%s) % EVERY))
+  (( seconds < 1 )) || sleep $((EVERY - seconds))
+done
