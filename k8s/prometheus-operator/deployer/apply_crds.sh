0068f9dbb8470950c499c54208ea321a64c8f36b
Armando Miani <armadom@google.com>
2022-05-10 11:35:59 +0200

    K8s - prometheus-operator - Fix release

--- k8s/prometheus-operator/deployer/apply_crds.sh
+++ k8s/prometheus-operator/deployer/apply_crds.sh
@@ -1,32 +1,25 @@
 #!/bin/bash
 
-declare -r VERSION=0.54.1
 declare -r REPO_URL="https://raw.githubusercontent.com/prometheus-operator/prometheus-operator"
 declare -r CRD_PATH="example/prometheus-operator-crd"
-
-# Apply CRDs
-kubectl apply -f "${REPO_URL}/v${VERSION}/${CRD_PATH}/monitoring.coreos.com_alertmanagerconfigs.yaml"
-kubectl apply -f "${REPO_URL}/v${VERSION}/${CRD_PATH}/monitoring.coreos.com_alertmanagers.yaml"
-kubectl apply -f "${REPO_URL}/v${VERSION}/${CRD_PATH}/monitoring.coreos.com_podmonitors.yaml"
-kubectl apply -f "${REPO_URL}/v${VERSION}/${CRD_PATH}/monitoring.coreos.com_probes.yaml"
-kubectl apply -f "${REPO_URL}/v${VERSION}/${CRD_PATH}/monitoring.coreos.com_prometheuses.yaml" \
-  --force-conflicts=true --server-side
-kubectl apply -f "${REPO_URL}/v${VERSION}/${CRD_PATH}/monitoring.coreos.com_prometheusrules.yaml"
-kubectl apply -f "${REPO_URL}/v${VERSION}/${CRD_PATH}/monitoring.coreos.com_servicemonitors.yaml"
-kubectl apply -f "${REPO_URL}/v${VERSION}/${CRD_PATH}/monitoring.coreos.com_thanosrulers.yaml"
-
-# Wait for resources
-crds=(
-  alertmanagerconfigs.monitoring.coreos.com
-  alertmanagers.monitoring.coreos.com
-  podmonitors.monitoring.coreos.com
-  probes.monitoring.coreos.com
-  prometheuses.monitoring.coreos.com
-  prometheusrules.monitoring.coreos.com
-  servicemonitors.monitoring.coreos.com
-  thanosrulers.monitoring.coreos.com
+declare -a CRDS=(
+  alertmanagerconfigs
+  alertmanagers
+  podmonitors
+  probes
+  prometheuses
+  prometheusrules
+  servicemonitors
+  thanosrulers
 )
 
-for crd in ${crds[@]}; do
-  kubectl wait --for=condition=Established --timeout=15s crd "${crd}"
+# Apply CRDs
+for crd in ${CRDS[@]}; do
+  kubectl apply -f "${REPO_URL}/v${VERSION}/${CRD_PATH}/monitoring.coreos.com_${crd}.yaml" \
+    --force-conflicts=true --server-side
+done
+
+# Wait for resources
+for crd in ${CRDS[@]}; do
+  kubectl wait --for=condition=Established --timeout=15s crd "${crd}.monitoring.coreos.com"
 done
