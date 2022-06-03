77b7361d58274e52dcd1a358a2c93d2068fa9caa
Armando Miani <armadom@google.com>
2022-06-03 11:50:07 +0200

    Fix tests

--- docker/spark/templates/3/start-spark.sh
+++ docker/spark/templates/3/start-spark.sh
@@ -14,21 +14,47 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -eu
+
 . "/opt/spark/bin/load-spark-env.sh"
+
+# Default values
+: ${SPARK_ENABLE_HISTORY:=false}
+: ${SPARK_ENABLE_PROMETHEUS:=false}
+
+declare -r config_file="/opt/spark/conf/spark-defaults.conf"
+
+# Configure Prometheus
+if [[ "${SPARK_ENABLE_PROMETHEUS}" == "true" ]]; then
+    cat <<EOF >> ${config_file}
+spark.eventLog.logStageExecutorMetrics=true
+spark.ui.prometheus.enabled=true
+EOF
+fi
+
+# Configure History Server
+if [[ "${SPARK_ENABLE_PROMETHEUS}" == "true" ]]; then
+    mkdir -p /opt/spark-events
+    cat <<EOF >> ${config_file}
+spark.eventLog.enabled=true
+spark.eventLog.dir=/opt/spark-events
+spark.history.fs.logDirectory=/opt/spark-events
+EOF
+fi
 
 cd /opt/spark/bin
 
 if [ "${SPARK_WORKLOAD}" == "master" ]; then
+    # Start master service
     export SPARK_MASTER_HOST=`hostname`
     ./spark-class org.apache.spark.deploy.master.Master \
         --host "${SPARK_MASTER_HOST}" \
         --port "${SPARK_MASTER_PORT}" \
         --webui-port ${SPARK_MASTER_WEBUI_PORT} >> ${SPARK_MASTER_LOG}
 elif [ "${SPARK_WORKLOAD}" == "worker" ]; then
+    # Start worker service
     ./spark-class org.apache.spark.deploy.worker.Worker \
         --webui-port ${SPARK_WORKER_WEBUI_PORT} ${SPARK_MASTER} >> ${SPARK_WORKER_LOG}
-elif [ "${SPARK_WORKLOAD}" == "submit" ]; then
-    echo "SPARK SUBMIT"
 else
     echo "Undefined Workload Type $SPARK_WORKLOAD, must specify: master, worker, submit"
 fi
