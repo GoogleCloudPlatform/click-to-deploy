7771201391b8bea6ede3fbad6eb25fe313eb6832
Armando Miani <armadom@google.com>
2022-08-05 23:57:02 +0200

    VM - WordPress-HA - Upgrade and Avoid exposing WP without setup (#1869)

--- vm/chef/cookbooks/wordpress-ha/attributes/default.rb
+++ vm/chef/cookbooks/wordpress-ha/attributes/default.rb
@@ -1,4 +1,4 @@
-# Copyright 2018 Google LLC
+# Copyright 2022 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,4 +16,8 @@ default['wordpress-ha']['user'] = 'www-data'
 default['wordpress-ha']['version'] = 'latest'
 default['wordpress-ha']['cli']['url'] = 'https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar'
 default['wordpress-ha']['temp_packages'] = ['unzip']
-default['wordpress-ha']['wp-stateless']['version'] = '2.3.2'
+default['wordpress-ha']['wp-stateless']['version'] = '3.2.2'
+default['wordpress-ha']['packages'] = [
+  'mysql-client',
+  'openssl',
+]
