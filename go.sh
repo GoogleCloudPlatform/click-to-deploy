99749dac7b3a93a188b4c46d4b2895a6e1c5e95e
Armando Miani <armadom@google.com>
2022-10-25 16:15:54 +0200

    Remove unused files

--- go.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-while IFS="/" read -r app_type solution path; do
-  # app_type="$(echo "${change}" | cut -d "/" -f 1)"
-  # solution="$(echo "${change}" | cut -d "/" -f 2)"
-  echo "(${app_type}) ${solution}"
-done < changes.txt
--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-gcloud beta builds submit \
-  --project cloud-marketplace-ops-test \
-  --substitutions=_GH_TOKEN=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 \
-  --config cloudbuild-docker-dispatcher.yaml
