d0b3b40a34f284f852dd2b4abe92690a3bf84822
Armando Miani <armadom@google.com>
2022-10-25 16:16:41 +0200

    Remove unused files

--- docker/dragonfly2/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker build -t dragonfly-dfget  2/debian11/dragonfly-dfget/2.0/
-docker build -t dragonfly-manager  2/debian11/dragonfly-manager/2.0/
-docker build -t dragonfly-scheduler  2/debian11/dragonfly-scheduler/2.0/
-docker build -t dragonfly-seed-peer  2/debian11/dragonfly-seed-peer/2.0/
