b7f6ac92159eb0fee04a48d1aff4859aead7725c
Armando Miani <armadom@google.com>
2022-11-17 22:17:46 +0100

    Remove tester folder

--- k8s/argo-workflows/tester/tests/01-submit-job.sh
+++
@@ -1,6 +0,0 @@
-argo submit \
-  --output json \
-  --wait https://raw.githubusercontent.com/argoproj/argo-workflows/master/examples/hello-world.yaml \
-  | jq -r '.metadata.name'
-
-argo get hello-world-g7d7x --output json | jq '.status.phase'
