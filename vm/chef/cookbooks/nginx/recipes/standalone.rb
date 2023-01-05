fcf3482fd4c3b4e2cf7511ec308c1178862787a9
Armando Miani <armadom@google.com>
2023-01-05 13:44:43 +0100

    VM - NGINX - Upgrade solution, Debian and MySQL

--- vm/chef/cookbooks/nginx/recipes/standalone.rb
+++ vm/chef/cookbooks/nginx/recipes/standalone.rb
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include_recipe 'mysql'
+include_recipe 'mysql::version-8.0'
 include_recipe 'php74'
 include_recipe 'php74::module_mysql'
 
