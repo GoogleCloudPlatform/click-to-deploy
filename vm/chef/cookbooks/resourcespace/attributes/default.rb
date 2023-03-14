607aa95d2d3d9a5ff28b36a39974ec2d9642544f
Adrian Manikowski <manikowski@google.com>
2023-03-14 12:53:58 +0000

    php81 updated

--- vm/chef/cookbooks/resourcespace/attributes/default.rb
+++ vm/chef/cookbooks/resourcespace/attributes/default.rb
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 default['resourcespace']['version'] = '10.1'
 
 default['resourcespace']['db']['name'] = 'resourcespace'
