0088f5084fd2b79b8871e86b4d05d85dfa416dfa
Armando Miani <armadom@google.com>
2023-03-20 12:51:02 +0100

    VM - DreamFactory - Upgrade to 4.14.1

--- vm/chef/cookbooks/dreamfactory/attributes/default.rb
+++ vm/chef/cookbooks/dreamfactory/attributes/default.rb
@@ -1,4 +1,4 @@
-# Copyright 2022 Google LLC
+# Copyright 2023 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,5 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-default['dreamfactory']['version'] = '4.12.0'
+default['dreamfactory']['version'] = '4.14.1'
 default['dreamfactory']['db']['name'] = 'dreamfactory'
+
+default['php81']['distribution'] = 'bullseye'
