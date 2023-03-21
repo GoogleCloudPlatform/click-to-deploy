90e12f94918e91a2a1c89ae93c29801daf66800a
Armando Miani <armadom@google.com>
2023-03-21 14:44:55 +0100

    Fix PHP version

--- vm/chef/cookbooks/lamp/recipes/default.rb
+++ vm/chef/cookbooks/lamp/recipes/default.rb
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+node.override['php81']['distribution'] = 'bullseye'
+
 include_recipe 'apache2'
 include_recipe 'apache2::security-config'
 include_recipe 'apache2::mod-rewrite'
