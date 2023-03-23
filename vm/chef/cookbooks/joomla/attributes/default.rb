3316fd08158b87760354df3090ca76494f588f14
Armando Miani <armadom@google.com>
2023-03-23 17:25:58 +0100

    VM - Joomla - Upgrade to 4.2.9 and PHP 8.1 (#2215)
    * VM - Joomla - Upgrade to 4.2.9 and PHP 8.1
    
    * Fix php path

--- vm/chef/cookbooks/joomla/attributes/default.rb
+++ vm/chef/cookbooks/joomla/attributes/default.rb
@@ -1,4 +1,4 @@
-# Copyright 2022 Google LLC
+# Copyright 2023 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,8 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-default['joomla']['version'] = '4.2.6'
-default['joomla']['sha256'] = '96db699bb056966f0e755522621f83a7161f651066ad4ed192d5d50071c1ce67'
+default['joomla']['version'] = '4.2.9'
+default['joomla']['sha256'] = '30c81431e5dcfeac30009925e9ff9c8e69e5dbe8f04a6d5a499f45163eb45fe9'
 default['joomla']['user'] = 'www-data'
-
 default['joomla']['db']['name'] = 'joomla_db'
+
+default['php81']['distribution'] = 'bullseye'
