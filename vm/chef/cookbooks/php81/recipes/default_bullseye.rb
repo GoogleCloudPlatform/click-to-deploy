e269081172026aa246b5098cdf89bd520cca3312
Adrian Manikowski <manikowski@google.com>
2023-03-24 16:02:10 +0000

    Updated startup

--- vm/chef/cookbooks/php81/recipes/default_bullseye.rb
+++ vm/chef/cookbooks/php81/recipes/default_bullseye.rb
@@ -1,4 +1,4 @@
-# Copyright 2021 Google LLC
+# Copyright 2023 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,18 +18,18 @@ apt_repository 'php' do
     key 'https://packages.sury.org/php/apt.gpg'
     components ['main']
   end
-  
+
   apt_update do
     action :update
   end
-  
+
   package 'install packages' do
     package_name node['php81']['packages']
     action :install
     retries 5
     retry_delay 20
   end
-  
+
   node['php81']['modules'].each do |pkg|
     include_recipe "php81::module_#{pkg}"
   end
