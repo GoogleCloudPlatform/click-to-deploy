704ea681a8fd87a9d1eec542704392bffe9e2265
Marcin Dobrucki <58482972+marzinkievitz@users.noreply.github.com>
2023-04-05 17:40:32 +0200

    Fix zabbix sql paths (#2265)

--- vm/chef/cookbooks/zabbix/recipes/default.rb
+++ vm/chef/cookbooks/zabbix/recipes/default.rb
@@ -58,8 +58,8 @@ RedirectMatch ^/$ /zabbix/
   su - postgres -c 'createdb -O zabbix -E Unicode zabbix'
   su - postgres -c 'createdb -O zabbix -E Unicode zabbix_proxy'
 
-  zcat /usr/share/doc/zabbix-sql-scripts/postgresql/server.sql.gz | sudo -u zabbix psql zabbix
-  cat /usr/share/doc/zabbix-sql-scripts/postgresql/proxy.sql | sudo -u zabbix psql zabbix_proxy
+  zcat /usr/share/zabbix-sql-scripts/postgresql/server.sql.gz | sudo -u zabbix psql zabbix
+  cat /usr/share/zabbix-sql-scripts/postgresql/proxy.sql | sudo -u zabbix psql zabbix_proxy
 
   ### for PostgreSQL -- uses socket (localhost uses tcp)
   sed -i 's/^# DBHost=localhost/DBHost=localhost/' /etc/zabbix/zabbix_server.conf
