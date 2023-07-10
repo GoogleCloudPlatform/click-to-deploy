ce599808cfa9f1fc2f379bca740a8678910a53ae
Armando Miani <armadom@google.com>
2023-07-10 12:37:49 +0200

    Add MySQL Licenses

--- vm/chef/cookbooks/mysql/recipes/version-8.0.rb
+++ vm/chef/cookbooks/mysql/recipes/version-8.0.rb
@@ -16,3 +16,25 @@
 
 include_recipe 'mysql::configure-apt-repo-version-8.0'
 include_recipe 'mysql::install-and-configure-mysqld8'
+
+ospo_download 'Licenses and Source-code' do
+  licenses <<-EOF
+MySQL8;https://github.com/mysql/mysql-server/blob/8.0/LICENSE
+curl;https://github.com/mysql/mysql-server/blob/8.0/extra/curl/curl-7.88.1/COPYING
+Duktape;https://github.com/mysql/mysql-server/blob/8.0/extra/duktape/duktape-2.7.0/LICENSE.txt
+googletest;https://github.com/google/googletest/blob/main/LICENSE
+libcbor;https://github.com/mysql/mysql-server/blob/8.0/extra/libcbor/LICENSE.md
+libedit;https://github.com/mysql/mysql-server/blob/8.0/extra/libedit/libedit-20210910-3.1/COPYING
+libevent;https://github.com/mysql/mysql-server/blob/8.0/extra/libevent/libevent-2.1.11-stable/LICENSE
+libfido;https://github.com/mysql/mysql-server/blob/8.0/extra/libfido2/libfido2-1.8.0/LICENSE
+lz4;https://github.com/mysql/mysql-server/blob/8.0/extra/lz4/lz4-1.9.4/LICENSE
+protobuf;https://github.com/mysql/mysql-server/blob/8.0/extra/protobuf/protobuf-3.19.4/LICENSE
+rapidjson;https://github.com/mysql/mysql-server/blob/8.0/extra/rapidjson/license.txt
+RobinhoodHashing;https://github.com/mysql/mysql-server/blob/8.0/extra/robin-hood-hashing/LICENSE
+zlib;https://github.com/mysql/mysql-server/blob/8.0/extra/zlib/zlib-1.2.13/LICENSE
+zstd;https://github.com/mysql/mysql-server/blob/8.0/extra/zstd/zstd-1.5.0/LICENSE
+EOF
+  ref_repos <<-EOF
+  https://github.com/mysql/mysql-server
+EOF
+end
