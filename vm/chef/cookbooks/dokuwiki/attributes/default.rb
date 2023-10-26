7199a31f575033c0f1c43c543c965e5caf91208a
Armando Miani <armadom@google.com>
2023-10-26 13:47:21 +0200

    VM - DokuWiki - Upgrade PHP, Dokuwiki and fix version (#2484)
    * VM - DokuWiki - Upgrade PHP, Dokuwiki and fix version
    
    * Fix language
    
    * Update php distribution apt repo
    
    * Fix startup script

--- vm/chef/cookbooks/dokuwiki/attributes/default.rb
+++ vm/chef/cookbooks/dokuwiki/attributes/default.rb
@@ -1,4 +1,4 @@
-# Copyright 2018 Google LLC
+# Copyright 2023 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,8 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+default['dokuwiki']['download_version'] = '2023-04-04'
 default['dokuwiki']['user']['name'] = 'admin'
 default['dokuwiki']['user']['real_name'] = 'John Doe'
 default['dokuwiki']['user']['groups'] = 'admin,user'
 
 default['dokuwiki']['title'] = 'DokuWiki!'
+
+default['php81']['distribution'] = 'bullseye'
