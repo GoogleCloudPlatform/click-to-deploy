5ea669dd83c35bd34ac311d4e2cf87697cc5774c
Armando Miani <armadom@google.com>
2023-11-02 19:58:27 +0100

    VM - Django - Update and fix version

--- vm/chef/cookbooks/django/attributes/default.rb
+++ vm/chef/cookbooks/django/attributes/default.rb
@@ -1,4 +1,4 @@
-# Copyright 2018 Google LLC
+# Copyright 2023 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,3 +16,4 @@
 # Reference: https://www.djangoproject.com/download/
 
 default['django']['packages'] = ['python3-pip']
+default['django']['version'] = '4.2.7'
--- vm/chef/cookbooks/django/recipes/default.rb
+++ vm/chef/cookbooks/django/recipes/default.rb
@@ -1,4 +1,4 @@
-# Copyright 2021 Google LLC
+# Copyright 2023 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -24,8 +24,11 @@ package node['django']['packages']
 bash 'install django via pip3' do
   user 'root'
   code <<-EOH
-    pip3 install django gunicorn
+    pip3 install gunicorn django=="${django_version}"
 EOH
+  environment({
+    'django_version': node['django']['version']
+  })
 end
 
 execute 'enable_apache_modules' do
