a7b44740013e0a49df6cd33a7b71508a6c8f4b7d
Armando Miani <armadom@google.com>
2024-02-29 16:07:59 +0100

    fix container

--- docker/airflow/templates/scripts/docker/install_airflow.sh
+++ docker/airflow/templates/scripts/docker/install_airflow.sh
@@ -10,7 +10,17 @@ function install_airflow() {
     echo
     echo "Installing all packages and upgrade if needed"
     echo
-    pip install --upgrade --upgrade-strategy only-if-needed "${AIRFLOW_INSTALLATION_METHOD}[${AIRFLOW_EXTRAS}]==${AIRFLOW_VERSION}" airflow-exporter 
+
+    # https://airflow.apache.org/docs/apache-airflow/stable/installation/installing-from-pypi.html
+    # https://github.com/apache/airflow/issues/36883
+    set -x
+    PY_MAJOR_VERSION="$(python3 -V | grep -o -P "\d+\.\d+")"
+    CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}-fix/constraints-${PY_MAJOR_VERSION}.txt"
+
+    pip install \
+        --constraint "${CONSTRAINT_URL}" \
+        --upgrade \
+        --upgrade-strategy only-if-needed "${AIRFLOW_INSTALLATION_METHOD}[${AIRFLOW_EXTRAS}]==${AIRFLOW_VERSION}" airflow-exporter
 }
 
 install_airflow
