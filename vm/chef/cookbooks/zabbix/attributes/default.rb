660f76ced106c00b8a901258418029ebaeff4120
Adrian Manikowski <manikowski@google.com>
2024-06-18 07:37:49 +0000

    VM - Zabbix BUMP to 7.0

--- vm/chef/cookbooks/zabbix/attributes/default.rb
+++ vm/chef/cookbooks/zabbix/attributes/default.rb
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-default['zabbix']['version'] = '6.5'
+default['zabbix']['version'] = '7.0'
 default['zabbix']['packages'] = %w(mailutils php-pgsql zabbix-server-pgsql zabbix-frontend-php zabbix-proxy-pgsql zabbix-sql-scripts zabbix-apache-conf php-gd php-bcmath php-mbstring php-xml php-ldap php-json)
 default['zabbix']['repo']['uri'] = "https://repo.zabbix.com/zabbix/#{default['zabbix']['version']}/debian/"
 default['zabbix']['repo']['components'] = ['main']
