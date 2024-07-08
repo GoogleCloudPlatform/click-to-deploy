0c6ce403ec96ecd5c0c84d91f7c8f547f659d79b
Armando Miani <armadom@google.com>
2024-07-08 15:00:28 +0200

    add ospo

--- vm/chef/cookbooks/zabbix/recipes/default.rb
+++ vm/chef/cookbooks/zabbix/recipes/default.rb
@@ -20,7 +20,7 @@ include_recipe 'apache2::default'
 include_recipe 'apache2::rm-index'
 include_recipe 'apache2::security-config'
 include_recipe 'postgresql::standalone_bookworm'
-# include_recipe 'zabbix::ospo'
+include_recipe 'zabbix::ospo'
 
 # install zabbix package
 apt_repository 'zabbix' do
