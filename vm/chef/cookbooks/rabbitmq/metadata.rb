9e7503d0976c51eccf4c0e40af874fdf1ed2ea7e
ynni <manikowski@google.com>
2024-07-10 08:53:33 +0200

    VM - Rabittmq - license and source-code added into the image (#2710)

--- vm/chef/cookbooks/rabbitmq/metadata.rb
+++ vm/chef/cookbooks/rabbitmq/metadata.rb
@@ -1,4 +1,4 @@
-# Copyright 2018 Google LLC
+# Copyright 2024 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,4 +13,6 @@
 # limitations under the License.
 
 name 'rabbitmq'
+depends 'c2d-config'
+depends 'c2d-shared'
 supports 'debian'
