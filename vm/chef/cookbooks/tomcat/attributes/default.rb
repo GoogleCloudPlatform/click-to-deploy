8bd1ea2435fb5e7ea84b85fea240309da7274940
Adrian Manikowski <manikowski@google.com>
2024-11-13 11:55:41 +0000

    sha256 changed for tomcat 11.0.0

--- vm/chef/cookbooks/tomcat/attributes/default.rb
+++ vm/chef/cookbooks/tomcat/attributes/default.rb
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 default['tomcat']['version'] = '11.0.0'
-default['tomcat']['sha256'] = '8427c5509f14e482940a015f3f60de20342d3d72c8dea9982a4d7112cb71a6ee'
+default['tomcat']['sha256'] = 'd0ca319af349838f59009a9c5ed3709f02344201059dbc26dce4313ee969cd20'
 
 default['tomcat']['app']['install_dir'] = '/opt/tomcat'
 
