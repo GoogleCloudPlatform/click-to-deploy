9cfa88daa19cfc54bdb265fc2bd78cd98887400b
Armando Miani <armadom@google.com>
2025-05-06 18:52:04 +0200

    Docker - Kafka - Bump to 3.9 (#2883)

--- docker/kafka/tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-UNIQUE="$RANDOM"
-
-# docker network create kafka
-docker run -d --network kafka --hostname zk-$UNIQUE-id -e ZOO_4LW_COMMANDS_WHITELIST="*" --name zk-$UNIQUE-id --restart always "marketplace.gcr.io/google/zookeeper3"
-docker run -d --network kafka --hostname kafka-$UNIQUE-id-1 -e "KAFKA_ZOOKEEPER_CONNECT=zk-$UNIQUE-id:2181" -e "KAFKA_ADVERTISED_HOST_NAME=kafka-$UNIQUE-id-1" -e "KAFKA_ADVERTISED_PORT=9092" -e "KAFKA_PORT=9092" --name kafka-$UNIQUE-id-1 kafka3:3.8
-docker run -d --network kafka --hostname kafka-$UNIQUE-id-2 -e "KAFKA_ZOOKEEPER_CONNECT=zk-$UNIQUE-id:2181" -e "KAFKA_ADVERTISED_HOST_NAME=kafka-$UNIQUE-id-2" -e "KAFKA_ADVERTISED_PORT=9092" -e "KAFKA_PORT=9092" --name kafka-$UNIQUE-id-2 kafka3:3.8
-sleep 5s
